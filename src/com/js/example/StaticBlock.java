f091dd00b7159fd1e397ed2e03b154fa38f4e10c
j222su <patrickstar0910@gmail.com>
2021-10-05 15:53:21 +0900

    ADD : create StaticBlock

---
+++ src/com/js/example/StaticBlock.java
@@ -0,0 +1,12 @@
+package com.js.example;
+
+public class StaticBlock {
+
+    static {
+        System.out.println("Static Block");
+    }
+
+     public StaticBlock(String str) {
+         System.out.println(str);
+     }
+}
